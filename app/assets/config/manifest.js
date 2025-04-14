2ff758fd9ad6e279857da9f3629d499fed4acc54
HUY <lehuy091100@gmail.com>
2025-04-14 14:45:13 +0700

    Fisnish lauout and routes

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+  //= link_tree ../images
+  //= link controllers/application.js
+  //= link_directory ../stylesheets .scss
+    //= link_directory ../stylesheets .css
+    //= link controllers/index.js
+//= link application.js
